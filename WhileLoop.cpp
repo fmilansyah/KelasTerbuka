770f3605c04e1a626b5ef27ddf27c58e202ff0af
Febri Milansyah <febrimilan00@gmail.com>
2022-11-04 23:43:52 +0700

    While Loop

---
+++ WhileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int WhileLoop()
+{
+	int num1 = 5;
+	while (num1 <= 10) {
+		cout << "hore " << num1 << endl;
+		num1++;
+	}
+
+	cout << "Selesai" << endl;
+	return 0;
+}
