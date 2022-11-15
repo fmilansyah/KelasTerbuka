60b74aa964df9e90d3bf61a034b6d2699784a318
Febri Milansyah <febrimilan00@gmail.com>
2022-11-15 21:56:56 +0700

    WIP : Triangle

---
+++ Triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cout << "Masukan Panjang Pola : ";
+	cin >> n;
+
+	cout << "Pola 3";
+	for (int i = 1; i <= n; i++) {
+		for (int j = )
+	}
+	return 0;
+}
