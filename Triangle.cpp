0790d8d41318f9a3799fb407b351a9eea298be8c
Febri Milansyah <febrimilan00@gmail.com>
2022-11-16 21:02:33 +0700

    Pola 1 & Pola 2

--- Triangle.cpp
+++ Triangle.cpp
@@ -9,9 +9,20 @@ int main()
 	cout << "Masukan Panjang Pola : ";
 	cin >> n;
 
-	cout << "Pola 3";
+	cout << "Pola 1" << endl;
 	for (int i = 1; i <= n; i++) {
-		for (int j = )
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	cout << "Pola 2" << endl;
+	for (int i = 1; i <= n; i++) {
+		for (int j = n; j >= i; j--) {
+			cout << "*";
+		}
+		cout << endl;
 	}
 	return 0;
 }
