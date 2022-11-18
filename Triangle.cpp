d6a9532b023a416000754eaeab6ead767bd9f864
Febri Milansyah <febrimilan00@gmail.com>
2022-11-18 22:38:29 +0700

    Pola 6

--- Triangle.cpp
+++ Triangle.cpp
@@ -57,5 +57,16 @@ int main()
 		}
 		cout << endl;
 	}
+
+	cout << "Pola 6" << endl;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			cout << " ";
+		}
+		for (int k = n; k >= (2*i-n); k--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
 	return 0;
 }
