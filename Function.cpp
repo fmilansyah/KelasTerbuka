57bdfaaa13efbeb5ee6479e42fbda94bfa4d2e73
Febri Milansyah <febrimilan00@gmail.com>
2022-11-21 13:29:34 +0700

    Function

---
+++ Function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int x;
+
+	cout << "Menghitung akar dari x : ";
+	cin >> x;
+
+	double y = sqrt(x);
+	cout << "Akarnya adalah " << y << endl;
+
+	return 0;
+}
